92a2cc37234aed54361138af0724b9bcedc325a8
Quinn Tenorio <quinnktenorio@gmail.com>
2021-12-27 10:25:00 -0700

    Structure changes

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function (eleventyConfig) {
+    return {
+        // These are all optional, defaults are shown:
+        dir: {
+            input: "src",
+            includes: "_includes",
+            data: "_data",
+            output: "_site",
+        },
+    };
+};
